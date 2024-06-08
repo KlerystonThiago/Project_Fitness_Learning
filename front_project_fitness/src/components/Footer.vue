97a409bbf4ca3cf09545e64308ba42b47af0d263
Kleryston Thiago <klerystony@hotmail.com>
2024-06-08 11:40:40 -0300

    Implementação da linha 3 do site.

--- front_project_fitness/src/components/Footer.vue
+++ front_project_fitness/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    FOOTER
+    
   </div>
 </template>
 
--- front_project_fitness/src/views/HomeView.vue
+++ front_project_fitness/src/views/HomeView.vue
@@ -1,12 +1,118 @@
 <template>
-    <main>
+    <main style="margin-bottom: 700px;">
+        <div class="mt_50 mb_50">
+            <div class="container">
+                <div class="box_line_title d_flex flex_direction_column justify_center align_items_center">
+                    <span class="f_weight_9">FITNESS</span>
+                    <div class="title_and_subtitle">
+                        <h2>The Largest Gym in the City</h2>                
+                        <p class="f_weight_5 f_size_12">
+                            We specialize in creating a custom, unique, and fuctional sapces by working with you to build your vision.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="d_flex flex_wrap">
+                    <div class="line_1_col w_25 p_10">
+                        <div class="line_1_card_box w_100 d_flex flex_direction_column justify_around align_items_center">
+                            <img src="../assets/images/ico_01.svg" alt="">
+                            <p class="f_weight_6 w_100">
+                                302 Training Device
+                            </p>
+                        </div>
+                    </div>
+                    <div class="line_1_col w_25 p_10">
+                        <div class="line_1_card_box w_100 d_flex flex_direction_column justify_around align_items_center">
+                            <img src="../assets/images/ico_02.svg" alt="">
+                            <p class="f_weight_6 w_100">
+                                302 Training Device
+                            </p>
+                        </div>
+                    </div>
+                    <div class="line_1_col w_25 p_10">
+                        <div class="line_1_card_box w_100 d_flex flex_direction_column justify_around align_items_center">
+                            <img src="../assets/images/ico_03.svg" alt="">
+                            <p class="f_weight_6 w_100">
+                                302 Training Device
+                            </p>
+                        </div>
+                    </div>
+                    <div class="line_1_col w_25 p_10">
+                        <div class="line_1_card_box w_100 d_flex flex_direction_column justify_around align_items_center">
+                            <img src="../assets/images/ico_04.svg" alt="">
+                            <p class="f_weight_6 w_100">
+                                302 Training Device
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt_50 mb_50">
+            <div class="container">
+                <section>
+                    <div class="line_card_02 row">
+                        <div class="col-xs-12 col-sm-12 col-lg-6">
+                            <div class="card_resume d_flex justify_center flex_direction_column align_items_center">
+                                <h2>Gym of The Prestigious Network of Fitness Clubs</h2>
+                                <p class="f_weight_6">From $50</p>
+                                <span class="water_mark f_weight_6">GYM</span>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-lg-6">
+                            <div class="card_images">
+                                <img src="../assets/images/img_01.jpg" alt="" class="img_front">
+                                <img src="../assets/images/img_02.jpg" alt="" class="img_back">
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                
+                <section class="mt_50 mb_50">
+                    <div class="line_card_02 row">
+                        <div class="col-xs-12 col-sm-12 col-lg-6 order">
+                            <div class="card_resume d_flex justify_center flex_direction_column align_items_center">
+                                <h2>Gym of The Prestigious Network of Fitness Clubs</h2>
+                                <p class="f_weight_6">From $50</p>
+                                <span class="water_mark f_weight_6">AEROBIC</span>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-lg-6">
+                            <div class="card_images">
+                                <img src="../assets/images/img_04.jpg" alt="" class="img_front">
+                                <img src="../assets/images/img_03.jpg" alt="" class="img_back">
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                
+                <section>
+                    <div class="line_card_02 row">
+                        <div class="col-xs-12 col-sm-12 col-lg-6">
+                            <div class="card_resume d_flex justify_center flex_direction_column align_items_center">
+                                <h2>Gym of The Prestigious Network of Fitness Clubs</h2>
+                                <p class="f_weight_6">From $50</p>
+                                <span class="water_mark f_weight_6">CROSS</span>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-lg-6">
+                            <div class="card_images">
+                                <img src="../assets/images/img_05.jpg" alt="" class="img_front">
+                                <img src="../assets/images/img_06.jpg" alt="" class="img_back">
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+
         
     </main>
 </template>
 
 <script setup>
-    import Header from '../components/Header.vue'
-    import Footer from '../components/Footer.vue'
-    import Navigation from '../components/Navgation.vue'
+
 </script>
 
